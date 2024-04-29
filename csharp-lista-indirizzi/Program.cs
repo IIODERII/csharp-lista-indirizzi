bb9065f21fcc90bf966387839340ce02681e1e9c
IIODERII <oderrisi@gmail.com>
2024-04-29 14:40:45 +0200

    consegna esercizio

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] texts = File.ReadAllLines("C://addresses.csv");
+            List<Address> addresses = new List<Address>();
+
+            for (int i = 1; i < texts.Length; i++)
+            {
+                string[] data = texts[i].Split(",");
+                try
+                {
+                    foreach(string s in data)
+                    {
+                        if (s.Length == 0)
+                            throw new Exception();
+                    }
+                   addresses.Add(new Address(data[0], data[1], data[2], data[3], data[4], int.Parse(data[5])));
+                }
+                catch (Exception e) {
+                    if(data.Length < 5)
+                    {
+                        Console.WriteLine("ATTENZIONE!! Sembra che alcuni campi non siano presenti!");
+                    }else if (data[4].Length > 2)
+                    {
+                        Console.WriteLine("ATTENZIONE!! La provincia può contenere al massimo due lettere!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            foreach(Address a in addresses)
+            {
+                Console.WriteLine($"\nNome: {a.Name},\nCognome: {a.Surname},\nVia: {a.Street},\nCittà: {a.City},\nProvincia: {a.Province},\nCAP: {a.ZIP}");
+            }
         }
     }
 }
